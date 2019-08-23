fe40c1ef274c6b82a55b1ccf83b9055dbbb1914d
mgiurca <mgiurca.mail@gmail.com>
2019-08-23 15:18:58 +0300

    almost first hand played

--- GameClasses.py
+++ GameClasses.py
@@ -1,16 +1,40 @@
 from random import shuffle
 
 
-def cardFight(card1, card2):
+def cardFight(deck1, deck2):
+    card1 = deck1.pop()
+    card2 = deck2.pop()
+
+    if card1 > card2:
+        print("castiga p1")
+        return 1
+    elif card1 < card2:
+        print("castiga p2")
+        return 2
+    else:
+        print("+1 mana")
+        return cardFight(deck1, deck2)
 
 
-def StartGame(p1, p2):
+# def StartGame(p1, p2):
 
 
 class Card:
     def __init__(self, no, cardType):
         self.number = no
         self.cardType = cardType
+
+    def __lt__(self, other):
+        if self.number < other.number:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self.number >= other.number:
+            return True
+        else:
+            return False
 
 
 class Deck:
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from GameClasses import Deck
+from GameClasses import Deck, cardFight
 from Player import Player
 
 deck = Deck()
@@ -9,10 +9,10 @@ deck.shuffleDeck()
 # print('\n')
 # for card in deck.cards:
 #     print(card.cardType + " " + str(card.number), end= ' | ')
-name = input("Player 1: Name?\n")
-player1 = Player(name)
-name = input("Player 2: Name?\n")
-player2 = Player(name)
+# name = input("Player 1: Name?\n")
+player1 = Player("Mirel")
+# name = input("Player 2: Name?\n")
+player2 = Player("Mihai")
 while True:
     try:
         player1.drawCards(deck)
@@ -34,3 +34,7 @@ print("{p2Name}: {p2CardsNO} cards|".format(p2Name=player2.name, p2CardsNO=playe
 # Inceputul jocului
 # Acesta se termina cand unul din jucatori ramane fara carti
 # Castigatorul se declara cel care inca are Cards in lista self.hand
+print(player1.hand.__len__())
+cardFight(player1.hand, player2.hand)
+
+print(player1.hand.__len__())
